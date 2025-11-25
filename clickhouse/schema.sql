d707515dfb44706d86a4f52122cbed1531c6f4fe
phamhoa2416 <phamviethoa2416@gmail.com>
2025-11-26 01:23:46 +0700

    fix: change fields to non-nullable

--- clickhouse/schema.sql
+++ clickhouse/schema.sql
@@ -5,14 +5,14 @@ CREATE TABLE ecommerce_events
 (
     event_time    DateTime,
     event_type    String,
-    product_id    Nullable(UInt64),
-    category_id   Nullable(UInt64),
+    product_id    UInt64,
+    category_id   UInt64,
     category_code String,
     brand         String,
-    price         Nullable(Float64),
-    user_id       Nullable(UInt64),
+    price         Float64,
+    user_id       UInt64,
     user_session  String
 ) ENGINE = MergeTree()
         PARTITION BY toYYYYMM(event_time)
-        ORDER BY (event_time);
+        ORDER BY (event_time, user_id);
 
