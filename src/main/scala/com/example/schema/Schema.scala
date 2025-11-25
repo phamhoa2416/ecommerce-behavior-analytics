81248383b5feca8aab7a7b4f23a21f2343f71ef3
phamhoa2416 <phamviethoa2416@gmail.com>
2025-11-26 01:22:13 +0700

    fix: change to string type

--- src/main/scala/com/example/schema/Schema.scala
+++ src/main/scala/com/example/schema/Schema.scala
@@ -5,14 +5,14 @@ import org.apache.spark.sql.types._
 object Schema {
   val schema: StructType = StructType(
     Seq(
-      StructField("event_time", TimestampType, nullable = true),
+      StructField("event_time", StringType, nullable = true),
       StructField("event_type", StringType, nullable = true),
-      StructField("product_id", LongType, nullable = true),
-      StructField("category_id", LongType, nullable = true),
+      StructField("product_id", StringType, nullable = true),
+      StructField("category_id", StringType, nullable = true),
       StructField("category_code", StringType, nullable = true),
       StructField("brand", StringType, nullable = true),
-      StructField("price", DoubleType, nullable = true),
-      StructField("user_id", LongType, nullable = true),
+      StructField("price", StringType, nullable = true),
+      StructField("user_id", StringType, nullable = true),
       StructField("user_session", StringType, nullable = true)
     )
   )
