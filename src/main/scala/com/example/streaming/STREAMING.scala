bc685bd1dae1ec958a982eccdc0f857ba6149df0
phamhoa2416 <phamviethoa2416@gmail.com>
2025-11-25 23:55:20 +0700

    feat: write Spark streaming job to load data into ClickHouse

--- src/main/scala/com/example/streaming/STREAMING.scala
+++ src/main/scala/com/example/streaming/STREAMING.scala
@@ -1,35 +1,57 @@
 package com.example.streaming
 
+import com.example.AppConfig
 import com.example.parser.Parser
 import com.example.schema.Schema
-import com.example.util.SparkUtils
-import org.apache.spark.sql.{Dataset, Row}
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.apache.spark.sql.streaming.StreamingQuery
 
+import java.util.Properties
+
 object STREAMING {
+  val kafkaBootstrapServer: String = AppConfig.KAFKA_BOOTSTRAP_SERVERS
+  val kafkaTopic: String = AppConfig.KAFKA_BATCH_TOPIC
+  val kafkaGroupId: String = AppConfig.KAFKA_GROUP_ID
+  val kafkaStartingOffsets: String = AppConfig.KAFKA_STARTING_OFFSETS
+
+  val clickhouseUrl: String = AppConfig.CLICKHOUSE_URL
+  val clickhouseUser: String = AppConfig.CLICKHOUSE_USER
+  val clickhousePassword: String = AppConfig.CLICKHOUSE_PASSWORD
+  val clickhouseTable: String = AppConfig.CLICKHOUSE_TABLE
+
 
   def main(args: Array[String]): Unit = {
-    val spark = SparkUtils.createSparkSession("Structured Streaming Consumer")
-    val schema = Schema.schema
+    val spark = SparkSession.builder()
+      .appName("Streaming")
+      .master("local[*]")
+      .getOrCreate()
 
-    val kafkaStream = spark.readStream
+    val kafkaDf = spark.readStream
       .format("kafka")
-      .options(SparkUtils.kafkaOptions)
+      .option("kafka.bootstrap.servers", kafkaBootstrapServer)
+      .option("subscribe", kafkaTopic)
+      .option("startingOffsets", kafkaStartingOffsets)
+      .option("failOnDataLoss", "false")
       .load()
 
-    val parsedStream = Parser.parseData(kafkaStream, schema)
+    val parsedStream = Parser.parseData(kafkaDf, Schema.schema)
 
-    val checkpointLocation = sys.env.getOrElse("CHECKPOINT_LOCATION", "/tmp/checkpoint_clickhouse")
+    val checkpointLocation = sys.env.getOrElse("CHECKPOINT_LOCATION", "/tmp/checkpoint")
+
+    val connection = new Properties()
+    connection.put("driver", "com.clickhouse.jdbc.ClickHouseDriver")
+    connection.put("user", clickhouseUser)
+    connection.put("password", clickhousePassword)
 
     val query: StreamingQuery = parsedStream.writeStream
       .outputMode("append")
       .option("checkpointLocation", checkpointLocation)
       .foreachBatch { (batchDF: Dataset[Row], _: Long) =>
-        batchDF.write
-          .format("jdbc")
-          .mode("append")
-          .options(SparkUtils.clickhouseOptions)
-          .save()
+        if (!batchDF.isEmpty) {
+          batchDF.write
+            .mode("append")
+            .jdbc(clickhouseUrl, clickhouseTable, connection)
+        }
       }
       .start()
 
