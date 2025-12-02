e3c3dac8779c2233c999713c5701bc9548643492
phamhoa2416 <phamviethoa2416@gmail.com>
2025-12-02 19:41:16 +0700

    feat: implement Spark utilities

---
+++ src/main/scala/com/example/util/SparkUtils.scala
@@ -0,0 +1,20 @@
+package com.example.util
+
+import com.example.AppConfig
+import org.apache.spark.sql.SparkSession
+
+object SparkUtils {
+  def createSparkSession(appName: String): SparkSession = {
+    val spark = SparkSession.builder()
+      .appName(appName)
+      .master(AppConfig.SPARK_MASTER)
+      .config("spark.sql.adaptive.enabled", "true")
+      .config("spark.sql.adaptive.coalescePartitions.enabled", "true")
+      .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+      .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+      .getOrCreate()
+
+    spark.conf.set("spark.sql.shuffle.partitions", AppConfig.SPARK_SHUFFLE_PARTITIONS)
+    spark
+  }
+}
